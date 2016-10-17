dd104b2ba356417d34289fa71b53bdcfd7372657
Ezra <eml36@duke.edu>
2016-10-16 21:08:51 -0400

    added commands folder

---
+++ src/BackEndCommands/Forward.java
@@ -0,0 +1,25 @@
+package BackEndCommands;
+
+import java.util.List;
+
+import BackEndInternalAPI.Command;
+
+public class Forward implements Command {
+	private List<String> arguments;
+	
+	public Forward() {
+		
+	}
+	@Override
+	public void makeActionFromCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getCommandType() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
