2d00c3fa0103965bd1999a101714a74779a47924
Robert Steilberg <rsteilberg@gmail.com>
2016-10-18 01:32:14 -0400

    implement reflection to get number arguments for each command

--- src/BackEndInternalAPI/TreeParser.java
+++ src/BackEndInternalAPI/TreeParser.java
@@ -1,5 +1,7 @@
 package BackEndInternalAPI;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -12,24 +14,41 @@ import BackEndCommands.Sum;
  */
 public class TreeParser {
 
-
 	private static Command getCommandObj(String cmd) {
-        CommandDetector detector = new CommandDetector();
-        detector.addPatterns("resources/languages/English"); //TODO get current language from GUI here
-        detector.addPatterns("resources/languages/Syntax");
-        System.out.println(String.format("%s : %s", cmd, detector.getSymbol(cmd)));
+		CommandDetector detector = new CommandDetector();
+		detector.addPatterns("resources/languages/English"); // TODO get current
+																// language from
+																// GUI here
+		detector.addPatterns("resources/languages/Syntax");
+		String commandName = detector.getSymbol(cmd);
+		System.out.println(commandName);
+		try {
+			Class<?> cmdObj = Class.forName("BackEndCommands." + commandName);
+			try {
+				Constructor<?> commandObjCtor = cmdObj.getDeclaredConstructor();
+				Object o = commandObjCtor.newInstance();
+				Method numArgumentsMethod = o.getClass().getMethod("numArguments", new Class[] {});
+				int numArguments = (Integer) numArgumentsMethod.invoke(o, new Object[] {});
+				System.out.println(numArguments);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("class not found");
+			e.printStackTrace();
+		}
 
-        return new Sum();
-		
+		return new Sum();
+
 	}
-	
+
 	public static void main(String[] args) {
-		getCommandObj("forward");
+		getCommandObj("sum");
 	}
-	
+
 	private ParserTreeNode buildParserTree(Iterator<String> cmdIter) {
 		String currCmd = cmdIter.next();
-		ParserTreeNode newChild = new ParserTreeNode(currCmd,getCommandObj(currCmd));
+		ParserTreeNode newChild = new ParserTreeNode(currCmd, getCommandObj(currCmd));
 		for (int i = 0; i < 1; i++) {
 			if (cmdIter.hasNext()) { // base case
 				newChild.children.add(buildParserTree(cmdIter));
