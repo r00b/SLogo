0230aaa0b21a271b4221e1dd5a4faf3e719ba7a1
Robert Steilberg <rsteilberg@gmail.com>
2016-10-23 22:44:34 -0400

    commands ready

--- src/BackEndExternalAPI/CommandParser.java
+++ src/BackEndExternalAPI/CommandParser.java
@@ -17,10 +17,14 @@ public class CommandParser { // TODO DE STATIC EVERYTHING
 
     private static final String SETTINGS_PATH = "resources/internal/Settings";
 
-    public static HashMap<String, Double> myVariables = new HashMap<String, Double>();
+    protected static HashMap<String, Double> myVariables = new HashMap<String, Double>();
     public static HashMap<String, LogoMethod> myMethods = new HashMap<String, LogoMethod>();
     public static HashMap<String, Double> myMethodVariables = new HashMap<String, Double>();
+    private static ObservableProperties myProperties;
 
+//    public CommandParser(ObservableProperties properties) {
+//        myProperties = properties;
+//    }
 
     /**
      * Determines if a specified command is indeed an executable Command
@@ -28,7 +32,7 @@ public class CommandParser { // TODO DE STATIC EVERYTHING
      *
      * @param command is the specified String command
      */
-    private static boolean notCommand(String[] command) {
+    private boolean notCommand(String[] command) {
         if (command[0].equals("")) { // this is an empty line
             return true;
         }
@@ -46,7 +50,7 @@ public class CommandParser { // TODO DE STATIC EVERYTHING
      *
      * @param command a string containing the commandType issued from the editor
      */
-    public static double getAction(String command) {
+    public double getAction(String command) {
         ResourceBundle settings = ResourceBundle.getBundle(SETTINGS_PATH);
         String[] commands = command.trim().split(settings.getString("Delimiter"));
 
@@ -54,25 +58,17 @@ public class CommandParser { // TODO DE STATIC EVERYTHING
             return 0.0;
         }
         ParseTreeBuilder builder = new ParseTreeBuilder();
-        ParseTreeExecutor executor = new ParseTreeExecutor();
+        ParseTreeExecutor executor = new ParseTreeExecutor();//myProperties);
         //  executor.executeTree(builder.initParseTree(commands)); // TODO LEAVE COMMENTED WHEN DEBUGGING
 
-
         ParseTreeNode root = builder.initParseTree(commands); // TODO DEBUGGING
-
-//        printTree(root);
-
-
         double result = executor.executeTree(root); // TODO DEBUGGING
         myMethodVariables.clear();
-        //   System.out.println("--------- PRINTING TREE ----------"); // TODO DEBUGGING
-//        System.out.println("----------------------------------");
         return result;
-//        return 0;
     }
 
-    public static void printTree(ParseTreeNode r) { // TODO DEBUGGING
-          r = r.getChild(0);
+    public void printTree(ParseTreeNode r) { // TODO DEBUGGING
+        r = r.getChild(0);
 //        System.out.println("VALUE: " + r.getValue()); // make sure workign
         System.out.println("COMMAND: " + r.getCommand());
 
@@ -81,9 +77,9 @@ public class CommandParser { // TODO DE STATIC EVERYTHING
 
     public static void main(String[] args) { // TODO DEBUGGING
 //        System.out.println(getAction("TO fuck [ :a :b ] [ product :a :b ]"));
-//        System.out.println("===============================================================");
+////        System.out.println("===============================================================");
 //          System.out.println(getAction("fuck 2 3"));
-//        System.out.println(getAction("fuck 5 3"));
+////        System.out.println(getAction("fuck 5 3"));
 //    System.out.println(getAction("FOR [ :i 10 20 2 ] [ sum :i 0 ]"));
 
     }
