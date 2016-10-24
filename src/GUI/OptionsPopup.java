25afabef70b5a65d912469c3de80b901a2344276
Grayson Wise <graysonwise3@gmail.com>
2016-10-24 01:15:06 -0400

    options now opens a new window with the newly selected options

--- src/GUI/OptionsPopup.java
+++ src/GUI/OptionsPopup.java
@@ -1,6 +1,7 @@
 package GUI;
 import Base.OptionsMenu;
 import FrontEndInternalAPI.Options;
+import GUIController.GUIManager;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
@@ -12,6 +13,8 @@ import javax.swing.text.html.Option;
  */
 public class OptionsPopup extends OptionsMenu {
 //    private Stage stage;
+    public GUIManager myGUI;
+
     public OptionsPopup(Stage s, Color penColor, String background, String turtle, String language){
         super(s);
         super.setDefaults(penColor, background, turtle, language);
@@ -55,10 +58,12 @@ public class OptionsPopup extends OptionsMenu {
         newButton.setOnMouseExited(e -> newButton.setStyle(getOverButton()));
         newButton.setTranslateX(300);
         newButton.setTranslateY(500);
-//        newButton.setOnMouseClicked(e -> setParameters());
+        newButton.setOnMouseClicked(e -> setParameters());
         getStartWindow().getChildren().add(newButton);
     }
     @Override
     public void initIDE(String background, String turtle) {
+        myGUI = new GUIManager(getPenColor().getValue(), background, turtle, getLanguageBox().getValue());
+        myGUI.init();
     }
 }
