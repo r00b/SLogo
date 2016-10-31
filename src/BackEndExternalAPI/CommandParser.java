94a7e25a246653f0357fdd1c7671e88e27f3581f
Robert Steilberg <rsteilberg@gmail.com>
2016-10-30 21:07:43 -0400

    refactor

--- src/BackEndExternalAPI/CommandParser.java
+++ src/BackEndExternalAPI/CommandParser.java
@@ -1,27 +1,27 @@
 package BackEndExternalAPI;
 
 import BackEndInternalAPI.*;
-//import com.sun.tools.javac.code.Attribute;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
  * @author Robert H. Steilberg II
  *         <p>
- *         This class interprets a Logo commandType and executes the action associated with it
+ *         This class interprets a Logo command and executes the action(s) associated with it
  *         using the specified arguments. This is done by recursively creating a parse tree
  *         and executing down the nodes of the parse tree, checking for errors along the way.
+ *
+ *         Dependencies: ParseTreeBuilder, ObservableProperties
  */
 public class CommandParser {
 
     private static ObservableProperties myProperties;
     private static HashMap<String, Double> myVariables;
-    private static HashMap<String, Double> myMethodVariables;
+    private static HashMap<String, Double> myMethodVariables; // temporary map for method variables
     private static HashMap<String, LogoMethod> myMethods;
-    private static Set<String> myErrors;
+    private static Set<String> myErrors; // will hold any found errors
 
     public CommandParser(ObservableProperties properties) {
         myProperties = properties;
@@ -36,26 +36,45 @@ public class CommandParser {
         return myVariables;
     }
 
+    /**
+     * Getter for set containing error messages
+     *
+     * @return the set containing error messages
+     */
     public Set<String> getErrors() {
         return myErrors;
     }
 
     /**
-     * Executes the cumulative action associated with a Logo commandType issued
-     * from the editor
+     * Removes empty commands from an inputted list of commands
      *
-     * @param command a string containing the commandType issued from the editor
+     * @param array is the array to sanitized
+     * @return a sanitized array with empty commands removed
+     */
+    private String[] sanitize(String[] array) {
+        ArrayList<String> toSanitize = new ArrayList<String>();
+        for (String command : array) {
+            if (!command.equals("")) toSanitize.add(command);
+        }
+        String[] sanitizedList = new String[toSanitize.size()];
+        toSanitize.toArray(sanitizedList);
+        return sanitizedList;
+    }
+
+    /**
+     * Executes the cumulative action associated with a Logo command issued
+     * from the GUI
+     *
+     * @param command a string containing the commands issued from the editor
      */
     public double getAction(String command) {
-        String[] commands = command.trim().split("\\p{Space}");
-        ParseTreeBuilder builder = new ParseTreeBuilder(myVariables, myMethodVariables, myMethods, myProperties);
-        ParseTreeNode root = builder.initParseTree(commands);
+        String[] commands = sanitize(command.trim().split("\\p{Space}"));
+        ParseTreeBuilder builder = new ParseTreeBuilder(myProperties, myVariables, myMethods, myMethodVariables);
+        ParseTreeNode parseTree = builder.buildNewParseTree(commands);
         myErrors = builder.getErrors();
-        if (myErrors.size() != 0) {
-            return 0.0;
-        }
-        double result = root.getCommandObj().executeCommand(root);
-        myMethodVariables.clear(); // clear temporary variables
+        if (myErrors.size() != 0) return 0.0;
+        double result = parseTree.getCommandObj().executeCommand(parseTree);
+        myMethodVariables.clear(); // clear temporary method variables
         return result;
     }
 }
