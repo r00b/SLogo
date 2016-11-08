e15eb225815ddaed42ff6a0a4ac627e1470b43f4
DELIA LI <DL202@duke.edu>
2016-11-08 00:36:02 -0500

    finished adding bugs and impressions to readme

--- src/GUIController/GUIVariables.java
+++ src/GUIController/GUIVariables.java
@@ -24,17 +24,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.stream.Collectors;
 
-/** Created by Delia on 10/15/2016.
- *
- *  This is the GUIVariables class in the GUIController package.
- *  This class controls the representation of variables in a table format and allows
- *  users to add new variables and edit existing ones.
- *  Users can type in the textfields and click the "Add" button to manually input
- *  variables. They can also do it by declaring a variable in the Editor.
- *  Both methods of adding will also modify variables if they already exist under
- *  that name.
- *  For the TableView object, a Variable class was created to generate the correct
- *  values in the columns.
+/**
+ * Created by Delia on 10/15/2016.
  */
 public class GUIVariables implements Variables {
     private static final int BACKDROP_X = 10;
